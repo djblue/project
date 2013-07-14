b122828736ab563a1f9c933b58a23143ad9b3f34
Chris Badahdah <a.s.badahdah@gmail.com>
2013-07-14 22:57:05 +0000

    Cleaned up some files.

--- public/javascripts/request.js
+++ public/javascripts/request.js
@@ -1,121 +1,94 @@
 /**
- * A basic jQuery widget for managing student requests.
- * Users will be able to navigate different subjects
- * and request aid for specific classes.
- * 
+ * A basic widget for managing student requests.  Users will be able to
+ * navigate different subjects and request aid for specific classes.
+ *
  * Author: Abdullah Badahdah
  * Email:  abadahda@asu.edu
+ *
  */
 
-;(function ($) { // widget closure
+;(function ($, _, Backbone) { // closure start
 
-    var defaults = {
-        list: [],
-        testing: false
-    };
-
-    // Plug-in constructor;
-    var Plugin = function ($el, options) {
-
-        this.$el = $el;
-        this.list = options.list;
-
-        delete options.list;
-
-        this.options = $.extend({}, defaults, options);
-
-        if (!this.options.testing) {
-            this.init();
+    var Course = Backbone.Model.extend({
+        initialize: function () {
         }
+    });  
 
-        return this; // Return the newly created object.
+    var Courses = Backbone.Collection.extend({
+        url: '/courses',
+        model: Course
+    });
 
-    };
-
-    // Public methods for the plug-in.
-    Plugin.prototype = {
-
-        // Initialize the widget by showing the main menu.
-        init: function () {
-            this.main_menu();
+    var CoursesView = Backbone.View.extend({
+        initialize: function () {
+            this.collection.fetch({ async: false});
         },
+        collection: new Courses(),
+        el: $('#body'),
+        template: _.template($('#sub_menu').html()),
+        render: function (id) {
+        
+            console.log(id);
 
-        // Display the main menu.
-        main_menu: function() {
-
-            this.$el.html(''); // Clear the widget container.
-            var that = this;   // Set a scope reference.
-
-            // Iterate through the class list, showing only the
-            // categories.
-            $(this.list).each(function (i, obj) {
-
-                // Initialize a new category button.
-                var $el = $("<div/>").addClass("category");
-                $el.html("<h2>"+obj.category+"</h2>");
-
-                // Add the event handler to navigate to sub-menu.
-                $el.click( (function (pre, classes) {
-                    return function () { 
-                        that.show_menu(pre, classes);
-                    }
-                })(obj.prefix, obj.classes) );
-
-                that.$el.append($el); // Actually show the main menu. 
-            });
-
-            this.$el.append("<div id=\"clear\"></div>");
-        },
-
-        show_menu: function(pre, classes) {
-
-            var that = this;
-            var $ul = $("<ul class=\"classes\"></ul>");
-            var button = $('<li>Go Back</li>');
-            var $li;
-
-
-            button.click(function () {
-                that.main_menu();
-            });
-
-            $ul.append(button);
-
-            for (var i = 0; i < classes.length; i++)
-            {
-                $li = $("<li>");
-                $li.html(pre+" "+classes[i].number+" : "+classes[i].title);
-                $li.click((function (data) { 
-                    return function () {
-                       alert(data.title); 
-                        that.main_menu();
-                    };
-                })(classes[i]));
-                $ul.append($li);
-            }
-
-           this.$el.html('');
-           this.$el.append($ul);
-        },
-
-        request: function() {
-            
-        },
-    };
-
-
-    // Expose jQuery plug-in.
-    $.fn.request = function (options) {
-        // The plugin hasn't already been applied
-        if (this.data('hasPane') != undefined) {
-            console.log('Pane has already been applied to' + this);
-            return false;
-        } else {
-            this.data('hasPane', true);
-            // breaks traditional jquery chaining
-            return new Plugin(this, options);
+            this.$el.html(this.template({ 
+                courses: this.collection.where({ subject_id: "51cb732211c522e27c000002" }) 
+            }));
         }
-    };
+    });
 
+    var Subject = Backbone.Model.extend({
+        initialize: function () {
+            this.set('link', '#subject/'+this.get('_id'));
+        }
+    });  
 
-})(jQuery); // end of widget
+    var Subjects = Backbone.Collection.extend({
+        url: '/subjects',
+        model: Subject
+    });
+
+    var SubjectView = Backbone.View.extend({
+        initialize: function () {
+            this.collection.fetch({async: false});
+        },
+        collection: new Subjects(),
+        el: $('#body'),
+        template: _.template($('#main_menu').html()),
+        render: function () {
+            this.$el.html(this.template({ 
+                subjects: this.collection.models 
+            }));
+        }
+    });
+
+    var MainView = Backbone.View.extend({
+        el: $('#body'),
+
+        events: {
+            'click .subject': 'view_subject',
+            'click .back': 'main_menu',
+            'click .ask': 'enqueue'
+        },
+        
+        subjects: new SubjectView(),
+        courses: new CoursesView(),
+        
+        enqueue: function (e) {
+            console.log($(e.currentTarget).data('id'));
+            this.subjects.render();
+        },
+
+        initialize: function () {
+            this.subjects.render();
+        },
+        main_menu: function () {
+            this.subjects.render();
+        },
+        view_subject: function (e) {
+            this.courses.render($(e.currentTarget).data('id'))
+        },
+    });
+
+    var main = new MainView();
+
+})(jQuery, _, Backbone); // closure end
