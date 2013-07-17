90e0773e192023d42fe5fca676b33ce52eccebdf
Chris Badahdah <a.s.badahdah@gmail.com>
2013-07-17 17:44:59 +0000

    Added dragable question tabs; thank you hammer!

--- public/javascripts/views/sidebar.js
+++ public/javascripts/views/sidebar.js
@@ -1,6 +1,6 @@
-define([ 'jquery', 'underscore', 'backbone', ], 
+define([ 'jquery', 'underscore', 'backbone', 'hammer'], 
 
-function ($, _, Backbone) {
+function ($, _, Backbone, Hammer) {
 
     var Question = Backbone.Model.extend(); 
 
@@ -17,11 +17,62 @@ function ($, _, Backbone) {
 
     return Backbone.View.extend({
         
+        events: {
+            'dragstart  .question': 'dragstart',
+            'drag':      'drag',
+            'dragend':   'dragend',
+            'completed  .question': 'completed',
+            'click button': 'click'
+        },
+
+        dragstart: function (e) {
+            // Begin a dragging event.
+            this.drag_el = $(e.currentTarget);
+            this.x = this.drag_el.offset().left;  
+        },
+
+        drag: function (e) {
+            // Prevent default browser response.
+            e.gesture.preventDefault();
+
+            if (this.drag_el.html() != e.currentTarget)
+            {
+                if (e.gesture.deltaX > 0) {
+                    this.drag_el.offset({
+                        left: this.x + e.gesture.deltaX
+                    });
+                }
+            } else {
+                this.drag_el.animate({left: this.x}, 'fast');
+            }
+
+        },
+
+        dragend: function (e) {
+
+            e.gesture.stopDetect();
+
+            // Reset object if threshold has not been met.
+            if (this.drag_el.offset().left < this.drag_el.width()) {
+                
+                // Restore element to its initial position.
+                this.drag_el.animate({left: this.x}, 'fast');
+
+            } else {
+
+                // Trigger completed event.
+                this.drag_el.trigger('completed'); 
+            }
+        },
+        
         template: _.template($('#sidebar').html()),
 
         collection: questions,
 
         initialize: function () {
+            // Initialize hammer plug-in for gestures.
+            this.$el.hammer();
+
             this.render();
         },
 
